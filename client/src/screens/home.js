3dbb7fefa00bdff76d0c021049530e8942959d4c
gbarn360 <gbarn360@gmail.com>
2023-01-30 13:03:55 -0500

    minor styling changes

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -18,27 +18,27 @@ export default function Home() {
   const [groups, setGroups] = useState([]); //used for displaying 1 on * chats
 
   useEffect(() => {
-    socket.emit("getMemberList") ; //get online/offline members when navigated to home page
+    socket.emit("getMemberList"); //get online/offline members when navigated to home page
 
   }, [])
 
 
-  socket.on("retrievedChatGroups", (singleChats,groupChats) => { //getting chats
+  socket.on("retrievedChatGroups", (singleChats, groupChats) => { //getting chats
     var members = [];
     var date;
     var groups = [];
     var room;
-    for(let i = 0;i<groupChats.length;i++) { //filtering out user name from member names
-      for(let j = 0;j<groupChats[i].members.length;j++) {
+    for (let i = 0; i < groupChats.length; i++) { //filtering out user name from member names
+      for (let j = 0; j < groupChats[i].members.length; j++) {
 
-          if(groupChats[i].members[j] != location.state.name){
+        if (groupChats[i].members[j] != location.state.name) {
 
-            members.push(groupChats[i].members[j]);
-            date = groupChats[i].date;
-            room = groupChats[i].room;
-          }
+          members.push(groupChats[i].members[j]);
+          date = groupChats[i].date;
+          room = groupChats[i].room;
+        }
       }
-      groups.push({members:members,date:date,room:room});
+      groups.push({ members: members, date: date, room: room });
       members = [];
     }
     setGroups(groups);  //update group chat groups
@@ -46,10 +46,10 @@ export default function Home() {
 
 
   });
- 
+
 
   socket.on("updatedChatGroups", () => { //get updated chat groups when user leaves group
-    socket.emit("getChatGroups", location.state.name, socket.id); 
+    socket.emit("getChatGroups", location.state.name, socket.id);
   })
 
 
@@ -68,15 +68,15 @@ export default function Home() {
     setOnlineUsers(online);
     setOfflineUsers(offline);
 
-    socket.emit("getChatGroups",location.state.name, socket.id); //get single and group chats
-    
+    socket.emit("getChatGroups", location.state.name, socket.id); //get single and group chats
+
   })
 
   socket.on("createdGroupRoom", rooms => { //returns a created room
 
-    socket.emit("getChatGroups",location.state.name, socket.id); //get chat groups 
+    socket.emit("getChatGroups", location.state.name, socket.id); //get chat groups 
   })
-  
+
 
   const logout = () => {
     socket.emit("logout", location.state.name);
@@ -88,32 +88,32 @@ export default function Home() {
       e.stopPropagation();
       return;
     }
-    if(groups.user != undefined)
+    if (groups.user != undefined)
       navigate("/home/directMessage", { state: { name: location.state.name, visitorName: groups.user } });
-    else    
-       navigate("/home/groupMessage", { state: { name: location.state.name, visitorName: groups.members, room:groups.room } });
-    
+    else
+      navigate("/home/groupMessage", { state: { name: location.state.name, visitorName: groups.members, room: groups.room } });
+
 
   };
 
-  const setCheckedUsers = ()=>{
+  const setCheckedUsers = () => {
     let checkboxes = document.querySelectorAll("input[type=checkbox]");
     let checkedUsers = [];
-    
-    checkboxes.forEach((checkbox,i) =>{
-      if(checkbox.checked == true){
+
+    checkboxes.forEach((checkbox, i) => {
+      if (checkbox.checked == true) {
         checkedUsers.push(checkboxes[i].name)
       }
     });
     let date = getDate();
     let modifiedDate = date.split(" ")[0];
 
-    socket.emit("createGroup",location.state.name, checkedUsers,modifiedDate);
-    
-    
+    socket.emit("createGroup", location.state.name, checkedUsers, modifiedDate);
+
+
   }
 
-  const getDate = ()=>{
+  const getDate = () => {
     const date = new Date();
 
     var hours = date.getHours();
@@ -125,16 +125,16 @@ export default function Home() {
     var minutes = date.getMinutes();
     minutes = minutes < 10 ? "0" + minutes : minutes;
 
-    return date.getMonth() + 1 + "/" + 
-           date.getDate() + "/" +
-           date.getFullYear() + " " + 
-           hours + ":" + 
-           minutes +  "" + 
-           amPM;
+    return date.getMonth() + 1 + "/" +
+      date.getDate() + "/" +
+      date.getFullYear() + " " +
+      hours + ":" +
+      minutes + "" +
+      amPM;
 
-}
-    
-    
+  }
+
+
 
   return (
     <div className='container'>
@@ -143,7 +143,7 @@ export default function Home() {
         <button className="logoutButton" onClick={() => logout()}>log out</button>
         <div className='online'>
           <h4>Online - {onlineUsers.length - 1}</h4>
-          {onlineUsers.filter(users => users.user != location.state.name).map(users => (<p onClick={() => { navigate("/home/directMessage", { state: { name: location.state.name, visitorName: users.user }}) }}>{users.user}</p>))}
+          {onlineUsers.filter(users => users.user != location.state.name).map(users => (<p onClick={() => { navigate("/home/directMessage", { state: { name: location.state.name, visitorName: users.user } }) }}>{users.user}</p>))}
         </div>
         <div className='offline'>
           <h4>Offline - {offlineUsers.length}</h4>
@@ -157,17 +157,17 @@ export default function Home() {
         </div>
 
         <div id="manageGroupModal" className="manageGroupModal">
-          <button className='exit' onClick={() => { document.getElementById("manageGroupModal").style.display = "none"; let checkboxes = document.querySelectorAll('input[type=checkbox]'); checkboxes.forEach(item=>item.checked = false)}}>X</button>
-          
+          <button className='exit' onClick={() => { document.getElementById("manageGroupModal").style.display = "none"; let checkboxes = document.querySelectorAll('input[type=checkbox]'); checkboxes.forEach(item => item.checked = false) }}>X</button>
+
           <div className='list'>
-            {onlineUsers.filter(users => users.user != location.state.name).map((user, index) => (<div className='displayUser'><input type="checkbox" id="checkboxUser" name={user.user}  /><p style={{ padding: "5px" }} >{user.user}</p></div>))}
-            {offlineUsers.map((user, index) => (<div className='displayUser'><input type="checkbox" name={user.user} /><p style={{ padding: "5px" }} >{user.user}</p></div>))}
+            {onlineUsers.filter(users => users.user != location.state.name).map((user, index) => (<div className='displayUser'><input type="checkbox" id="checkboxUser" name={user.user} /><p style={{ padding: "5px", color: "#d4d4d4" }} >{user.user}</p></div>))}
+            {offlineUsers.map((user, index) => (<div className='displayUser'><input type="checkbox" name={user.user} /><p style={{ padding: "5px", color: "#d4d4d4" }} >{user.user}</p></div>))}
           </div>
-          <button className='button' id='createGroup' onClick={() => { document.getElementById("manageGroupModal").style.display = "none"; setCheckedUsers(); let checkboxes = document.querySelectorAll('input[type=checkbox]'); checkboxes.forEach(item=>item.checked = false)}}>Create Group</button>
+          <button className='button' id='createGroup' onClick={() => { document.getElementById("manageGroupModal").style.display = "none"; setCheckedUsers(); let checkboxes = document.querySelectorAll('input[type=checkbox]'); checkboxes.forEach(item => item.checked = false) }}>Create Group</button>
 
         </div>
-        {groups.map(groups => (<div onClick={(event) => navigateToChat(event, groups)} className="chatGroup">{groups.members + ""} <p className='chatGroupDate'>Created: {groups.date}</p><button className='button' id="leaveGroup" onClick={() => {  socket.emit("leaveGroup",location.state.name, groups.room,"groupChat") }}>leave group</button></div>))}
-        {chatGroups.map(groups => (<div onClick={(event) => navigateToChat(event, groups)} className="chatGroup" >{groups.user} <p className='chatGroupDate'>Created: {groups.date}</p><button className='button' id="leaveGroup" onClick={() => { socket.emit("leaveGroup",location.state.name, groups.room,"singleChat") }}>leave group</button></div>))}
+        {groups.map(groups => (<div onClick={(event) => navigateToChat(event, groups)} className="chatGroup">{groups.members + ""} <p className='chatGroupDate'>Created: {groups.date}</p><button className='button' id="leaveGroup" onClick={() => { socket.emit("leaveGroup", location.state.name, groups.room, "groupChat") }}>leave group</button></div>))}
+        {chatGroups.map(groups => (<div onClick={(event) => navigateToChat(event, groups)} className="chatGroup" >{groups.user} <p className='chatGroupDate'>Created: {groups.date}</p><button className='button' id="leaveGroup" onClick={() => { socket.emit("leaveGroup", location.state.name, groups.room, "singleChat") }}>leave group</button></div>))}
       </div>
       <div className='homeFiller'>
 
